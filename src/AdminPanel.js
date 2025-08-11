64bf9ddccdee59e74ddccfa139e3c512c2f7e58e
lilebrian <brian.lile@salesfireconsulting.com>
2025-08-11 13:19:49 -0400

    Update AdminPanel.js

--- src/AdminPanel.js
+++ src/AdminPanel.js
@@ -6,7 +6,7 @@ const months = [
   "Jan 2025", "Feb 2025", "Mar 2025", "Apr 2025", "May 2025", "Jun 2025",
   "Jul 2025", "Aug 2025", "Sep 2025", "Oct 2025", "Nov 2025", "Dec 2025"
 ];
-const personas = ["Operations", "Project Management", "HR/Talent Acquisition"];
+const personas = ["BioTech High Priority", "BioTech Low Priority", "Sustainability/Greentech High Priority", "Sustainability/Greentech Low Priority"];
 
 export default function AdminPanel() {
   const { data, updateData } = useData();
